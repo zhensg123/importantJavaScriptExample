367865dbf092da86a5072c720a939bed65ce5338
zhensg123 <xyqblog@163.com>
2021-06-19 15:01:58 +0800

    '设计模式总结'

--- src/router/modules/dpattern.js
+++ src/router/modules/dpattern.js
@@ -1,7 +1,15 @@
 export default [{
-  path: '/dpattern/drag-component',
+  path: '/dpattern/single',
   name: 'dpattern',
   component: () => import(/* webpackChunkName: "dpattern" */ '@/views/dpattern/single.vue')
+},{
+  path: '/dpattern/observer',
+  name: 'dpattern',
+  component: () => import(/* webpackChunkName: "dpattern" */ '@/views/dpattern/observer.vue')
+},{
+  path: '/dpattern/publish-subscribe',
+  name: 'dpattern',
+  component: () => import(/* webpackChunkName: "dpattern" */ '@/views/dpattern/publish-subscribe.vue')
 }]
 
 // carousel
