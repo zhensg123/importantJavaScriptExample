3100eba982e44c10ce5418e1d66520debf26799b
zhensg123 <xyqblog@163.com>
2021-06-20 20:22:46 +0800

    '提交'

--- src/views/function/promise-cache.vue
+++ src/views/function/promise-cache.vue
@@ -8,13 +8,18 @@ export default {
   data () {
     return {
       code: `
-        // eslint-disable-next-line no-extend-native
-    Array.prototype.map = Array.prototype.map || function (fn) {
-      var result = []
-      for (let i = 0; i < this.length; i++) {
-        result.push(fn(this[i], i))
+    const userPromisesCache = new Map()
+    const request = {
+      get () {
+        return 1
       }
-      return result
+    }
+    const getUserById = function (userId) {
+      if (!userPromisesCache.has(userId)) {
+        const userPromise = request.get('https://users-service/v1/' + userId)
+        userPromisesCache.set(userId, userPromise)
+      }
+      return userPromisesCache.get(userId)
     }`,
       title: '自定义一个map'
     }
@@ -28,7 +33,7 @@ export default {
     }
     const getUserById = function (userId) {
       if (!userPromisesCache.has(userId)) {
-        const userPromise = request.get(`https://users-service/v1/${userId}`)
+        const userPromise = request.get('https://users-service/v1/' + userId)
         userPromisesCache.set(userId, userPromise)
       }
       return userPromisesCache.get(userId)
--- src/views/function/self-setinterval.vue
+++ src/views/function/self-setinterval.vue
@@ -8,13 +8,25 @@ export default {
   data () {
     return {
       code: `
-        // eslint-disable-next-line no-extend-native
-    Array.prototype.map = Array.prototype.map || function (fn) {
-      var result = []
-      for (let i = 0; i < this.length; i++) {
-        result.push(fn(this[i], i))
+        // 使用while(true)
+    function selfSetInterVal1 (fn, time) {
+      let start = +new Date()
+      return function () {
+        while (true) {
+          if (+new Date() - start >= time) {
+            fn()
+            start = +new Date()
+          }
+        }
       }
-      return result
+    }
+
+    function selfSetInterVal2 (fn, time) {
+      function inner () {
+        fn()
+        setTimeout(inner, time)
+      }
+      inner()
     }`,
       title: '自定义循环或者setInterVal'
     }
