25758498a9d985386967b53a1dea90b890001b27
zhenshigang <zhenshigang@xdf.cn>
2023-10-20 09:40:40 +0800

    提交

--- src/config/nav.js
+++ src/config/nav.js
@@ -1,63 +1,46 @@
-const files = require.context('../router/modules', false, /\.js$/)
 
-let configRoutes = []
-/**
- * inject routers
- */
-files
-  .keys()
-  .forEach(key => {
-    configRoutes = configRoutes
-      .concat(files(key).default)
-      .sort((a, b) => (a.sort ? a.sort - b.sort : -1)) // 读取出文件中的default模块
-  })
-
-const menus = {
-  function: {
-    name: '函数',
-    icon: 'icon-hanshu'
-  },
-  component: {
-    name: '组件',
-    icon: 'icon-zujian'
-  },
-  data: {
-    name: '数据',
-    icon: 'icon-82'
-  },
-  dpattern: {
-    name: '设计模式',
-    icon: 'icon-iconset0357'
-  },
-  css: {
-    name: 'css',
-    icon: 'icon-liulanqi'
-  },
-  browser: {
-    name: '浏览器',
-    icon: 'icon-liulanqi'
-  },
-  tool: {
-    name: '工具',
-    icon: 'icon-liulanqi'
-  },
-  vuek: {
-    name: 'vue知识点',
-    icon: 'icon-liulanqi'
-  },
-  practice: {
-    name: '实践',
-    icon: 'icon-liulanqi'
+var obj = {
+  a: {
+    b: {
+      d: {
+        f: 22
+      }
+    },
+    c: {
+      e: {
+        g: 33
+      }
+    }
   }
 }
-const menuArr = Object.keys(menus).map((menu) => {
-  const { name, icon } = menus[menu]
-  const temp = {
-    menu: name,
-    icon,
-    children: configRoutes.filter((route) => route.name === menu)
-  }
-  return temp
-})
 
-export default menuArr
+const BFSARR = []
+// Breadth First Search
+function BFS (obj) {
+  const undo = []
+  if (obj === null || typeof obj !== 'object') return
+  undo.unshift(obj)
+  while (undo.length) {
+    const item = undo.shift()
+    Object.entries(item).map(([key, val]) => {
+      BFSARR.push(key)
+      undo.push(val)
+    })
+  };
+  return BFSARR
+}
+console.log(BFS(obj))
+
+const DFSARR = []
+// Depth First Search
+function DFS (obj) {
+  if (obj === null || typeof obj !== 'object') return
+  Object.entries(obj).map(([k, v], index) => {
+    DFSARR.push(k)
+    if (typeof v === 'object') {
+      DFS(v)
+    }
+  })
+  return DFSARR
+}
+console.log(DFS(obj), 'newObj')
